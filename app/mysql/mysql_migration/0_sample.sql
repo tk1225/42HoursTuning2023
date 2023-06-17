874060ed17d54ad0be2a35c362684ae0797caedb
K4-R4 <mabdman512@gmail.com>
2023-06-17 13:39:35 +0900

    fixed

--- app/mysql/mysql_migration/0_sample.sql
+++
@@ -1,3 +0,0 @@
-ALTER  TABLE department ADD INDEX department_name_index (department_name);
-ALTER TABLE department_role_member ADD INDEX department_id_index (department_id);
-ALTER TABLE department_role_member ADD INDEX user_id_index (user_id);
