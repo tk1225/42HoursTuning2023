e620ba623168f19946c7e0514230ae7cb9b9e24f
K4-R4 <mabdman512@gmail.com>
2023-06-17 20:41:16 +0900

    heiretu

--- app/node/src/index.ts
+++ app/node/src/index.ts
@@ -1,6 +1,15 @@
 import { app } from "./app";
+import cluster from "cluster";
+import os from "os";
 
 const port = 8000;
+const numCPUs = os.cpus().length;
+if(cluster.isPrimary) {
+	for(let i = 0; i < numCPUs; i++){
+		cluster.fork();
+	}
+}else{
 app.listen(port, () => {
   console.log(`ポート${port}番で起動しました。`);
 });
+}
