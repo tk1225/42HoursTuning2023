8f77d56c29a769bd439441f87428706f27bb17be
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2023-06-17 21:56:46 +0900

    fix: image
    Commit type: (Choose one of the following)
    
    Scope: (A brief description of the part of the codebase affected by the commit)
    
    Long description (wrap at 72 characters):
    
    Motivation:
    
    Issue or Pull Request: (e.g., "fixes: #123" or "closes: #456" or "Issue # 789", "PR #1000")

--- image_convert.sh
+++ image_convert.sh
@@ -5,8 +5,8 @@ file_name=("dog.png" "dragon.png" "goat.png" "horse.png" "monkey.png" "ox.png" "
 input_directory="images/user-icon/origin_img"  # 入力ディレクトリのパス
 output_directory="images/user-icon"  # 出力ディレクトリのパス
 
-resize_width=200
-resize_height=200
+resize_width=500
+resize_height=500
 
 mkdir -p "$output_directory"
 
@@ -16,6 +16,12 @@ do
     input_path="${input_directory}/${file_name[i]}"  # 入力ファイルのパス
     output_path="${output_directory}/${file_name[i]}"  # 出力ファイルのパス
 
-    convert "$input_path" -strip -resize "${resize_width}x${resize_height}" -dither None -posterize 8 -colors 256 "$output_path"
+    # convert "$input_path" -strip -resize "${resize_width}x${resize_height}" -dither None -posterize 8 -colors 256 "$output_path"
+    convert "$input_path" -strip -resize "${resize_width}x${resize_height}" -dither None -posterize 4 -colors 4 -depth 4 "$output_path"
+
+
 done
 
+#     convert "images/user-icon/origin_img/pig.png" -strip -resize "${resize_width}x${resize_height}" -dither None -posterize 4 -colors 16 -depth 4 "images/user-icon/pig.png"
+#     convert "images/user-icon/origin_img/rat.png" -strip -resize "${resize_width}x${resize_height}" -dither None -posterize 4 -colors 16 -depth 16 "images/user-icon/rat.png"
+#     convert "images/user-icon/origin_img/snake.png" -strip -resize "${resize_width}x${resize_height}" -dither None -posterize 4 -colors 8 -depth 4 "images/user-icon/snake.png"
