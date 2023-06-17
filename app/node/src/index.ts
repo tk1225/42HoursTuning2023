96042746d15f6dd0ce1af597b8cd70b0a88c614e
K4-R4 <mabdman512@gmail.com>
2023-06-17 22:12:01 +0900

    multithread

--- app/node/src/index.ts
+++ app/node/src/index.ts
@@ -1,6 +1,25 @@
-import { app } from "./app";
+// import { app } from "./app";
+import express from "express";
+import cluster from "cluster";
+import os from "os";
 
 const port = 8000;
-app.listen(port, () => {
-  console.log(`ポート${port}番で起動しました。`);
-});
+const numCPUs = os.cpus().length;
+if (cluster.isPrimary){
+	console.log("Master");
+
+	for(let i = 0; i < numCPUs; i++){
+		cluster.fork();
+	}
+	cluster.on('exit', (worker, code, signal) => {
+		cluster.fork();
+	})
+}else {
+	import("./app").then((app: express.Express) => {
+		app.listen(port, () => {
+			console.log(`ポート${port}番で起動しました。`);
+		})
+}).catch((error) => {
+		console.log(error);
+	});
+}
