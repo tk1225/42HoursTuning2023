32eeb753132ed858ff3971aecd9e55dabd0bcab5
K4-R4 <mabdman512@gmail.com>
2023-06-18 12:10:08 +0900

    added index

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,13 +1,34 @@
+CREATE INDEX idx_file_id ON file (file_id);
+
+CREATE INDEX idx_skill_name ON skill (skill_name);
+CREATE INDEX idx_skill_id ON skill (skill_id);
+
+CREATE INDEX idx_office_id ON office (office_id);
+
+CREATE INDEX idx_file_id ON file (file_id);
+
 CREATE INDEX idx_user_id ON user (user_id);
+CREATE INDEX idx_user_name ON user (user_name);
+CREATE INDEX idx_kana ON user (kana);
 CREATE INDEX idx_mail ON user (mail);
 CREATE INDEX idx_password ON user (password);
 CREATE INDEX idx_employee_id ON user (employee_id);
 CREATE INDEX idx_office_id ON user (office_id);
+CREATE INDEX idx_goal ON user (goal_id);
+
+CREATE INDEX idx_role_name ON role (role_name);
+
 CREATE INDEX idx_department_name ON department (department_name);
 CREATE INDEX idx_department_id ON department (department_id);
+
 CREATE INDEX idx_user_id ON match_group_member (user_id);
 CREATE INDEX idx_match_group_id ON match_group_member (match_group_id);
+
+CREATE INDEX idx_match_group_id ON match_group (match_group_id);
+
 CREATE INDEX idx_department_id ON department_role_member (department_id);
 CREATE INDEX idx_role_id ON department_role_member (role_id);
 CREATE INDEX idx_user_id ON department_role_member (user_id);
+
 CREATE INDEX idx_linked_user_id ON session (linked_user_id);
+CREATE INDEX idx_session_id ON session (session_id);
