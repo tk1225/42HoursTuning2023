d166a97833b2d40d6e5efa18b79e732e31a96903
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2023-06-18 11:38:43 +0900

    feat: "ファイル情報のキャッシュ機構を追加し、データベースへのアクセスを最適化"

--- app/node/src/routes/files/repository.ts
+++ app/node/src/routes/files/repository.ts
@@ -2,19 +2,34 @@ import { RowDataPacket } from "mysql2";
 import pool from "../../util/mysql";
 import { File } from "../../model/types";
 
+// キャッシュの準備
+const fileCache: { [key: string]: File } = {};
+
 export const getFileByFileId = async (
   fileId: string
 ): Promise<File | undefined> => {
+
+  // キャッシュに存在する場合はそれを返す
+  if (fileCache[fileId]) {
+    return fileCache[fileId];
+  }
+
   const [file] = await pool.query<RowDataPacket[]>(
     "SELECT file_name, path FROM file WHERE file_id = ?",
     [fileId]
   );
+  
   if (file.length === 0) {
     return;
   }
 
-  return {
+  const result = {
     fileName: file[0].file_name,
     path: file[0].path,
   };
+
+  // キャッシュに保存
+  fileCache[fileId] = result;
+
+  return result;
 };
