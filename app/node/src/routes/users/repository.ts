065fd6c28d28e7f91954f1b8dd62cf5522ab524d
Takumasa Okamoto <takumasaokamoto@TakumasanoMacBook-Pro.local>
2023-06-18 12:48:59 +0900

    getUserByIDのみ変更

--- app/node/src/routes/users/repository.ts
+++ app/node/src/routes/users/repository.ts
@@ -81,30 +81,24 @@ export const getUserByUserId = async (
 export const getUsersByUserIds = async (
   userIds: string[]
 ): Promise<SearchedUser[]> => {
-  let users: SearchedUser[] = [];
-  for (const userId of userIds) {
-    const [userRows] = await pool.query<RowDataPacket[]>(
-      "SELECT user_id, user_name, kana, entry_date, office_id, user_icon_id FROM user WHERE user_id = ?",
-      [userId]
-    );
-    if (userRows.length === 0) {
-      continue;
-    }
-
-    const [officeRows] = await pool.query<RowDataPacket[]>(
-      `SELECT office_name FROM office WHERE office_id = ?`,
-      [userRows[0].office_id]
-    );
-    const [fileRows] = await pool.query<RowDataPacket[]>(
-      `SELECT file_name FROM file WHERE file_id = ?`,
-      [userRows[0].user_icon_id]
-    );
-    userRows[0].office_name = officeRows[0].office_name;
-    userRows[0].file_name = fileRows[0].file_name;
-
-    users = users.concat(convertToSearchedUser(userRows));
+  if (userIds.length === 0) {
+    return [];
   }
-  return users;
+  const userIdsString = userIds.join(', ');
+
+  const query = `
+    SELECT
+      u.user_id, u.user_name, u.kana, u.entry_date,
+      u.office_id, u.user_icon_id, o.office_name, f.file_name
+    FROM user u
+    JOIN office o ON u.office_id = o.office_id
+    JOIN file f ON u.user_icon_id = f.file_id
+    WHERE u.user_id IN (${userIdsString})
+  `;
+
+  const [rows] = await pool.query<RowDataPacket[]>(query);
+
+  return convertToSearchedUser(rows);
 };
 
 export const getUsersByUserName = async (
